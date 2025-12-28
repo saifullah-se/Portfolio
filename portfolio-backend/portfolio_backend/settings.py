edb465c3adadc744a61ac1040511261146c14b75
Saif Ullah <saifullahbhatti.se@gmail.com>
2025-12-28 23:41:33 +0500

    Fix CORS trailing slash and database link

--- portfolio-backend/portfolio_backend/settings.py
+++ portfolio-backend/portfolio_backend/settings.py
@@ -39,13 +39,13 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    'https://saifullah-portfolio-b.up.railway.app', # Correct
-    'https://saifullah-portfolio.onrender.com/'
+    'https://saifullah-portfolio.onrender.com', 
+    'https://saifullah-portfolio-b.up.railway.app',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://saifullah-portfolio-b.up.railway.app', # Correct
-    'https://saifullah-portfolio.onrender.com/'
+    'https://saifullah-portfolio.onrender.com',
+    'https://saifullah-portfolio-b.up.railway.app',
 ]
 
 ROOT_URLCONF = "portfolio_backend.urls"
