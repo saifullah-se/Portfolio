e3889bfcd7b57af0e88b8f27386d89b6c35e2439
Saif Ullah <saifullahbhatti.se@gmail.com>
2025-12-29 03:43:53 +0500

    Fixed Export

--- saifullah-portfolio/src/api.js
+++ saifullah-portfolio/src/api.js
@@ -1,12 +1,11 @@
+// src/api.js
 import axios from 'axios';
 
-// Temporarily hardcode the live URL to bypass any Env Var issues
+// FORCE the live URL here
 const baseURL = 'https://saifullah-portfolio-b.up.railway.app'; 
 
 const api = axios.create({
   baseURL: `${baseURL}/api/`
 });
-
-export const MEDIA_URL = baseURL;
 
 export default api;
